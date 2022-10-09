09da6b7b3f93cd95473a77d4321e9efa62dc9145
Samyak1202 <99115614+Samyak1202@users.noreply.github.com>
2022-10-09 17:43:49 +0530

    Secret key

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b1$dtu1xpll793dafzl4$2yk7f3!ea=4m21y3v+gl00!1)l!10'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
